a3eb0c3ccdd836191999885074cce94810e87cdb
michael <michael.lee.esparza@gmail.com>
2019-01-24 22:28:04 -0800

    add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -158,7 +158,14 @@ app.post("/delete", function(req, res) {
 
 });
 
+//for heroku servers
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
